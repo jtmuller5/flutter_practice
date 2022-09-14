e8271c1e30e01ba10b1314b278de051b36d7fb20
Joseph Muller <jtmuller5@gmail.com>
2022-09-14 16:18:46 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,55 +14,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
                                                                                 primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-      
-        
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-                                    _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-                            return Scaffold(
-      appBar: AppBar(
-                        title: Text(widget.title),
-      ),
-      body: Center(
-                        child: Column(
-                                                                                                                                                      mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),     );
+    return Container();
   }
 }
+
