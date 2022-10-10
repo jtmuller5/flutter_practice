4b352ef4f4fd927bb3ea0af24e4973196e538fba
Danielle Kom <97759611+daniellekom@users.noreply.github.com>
2022-10-10 16:48:37 -0400

    wip

--- lib/flossy_screen/flossy_screen_view.dart
+++ lib/flossy_screen/flossy_screen_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
 
 class FlossyScreenView extends StatelessWidget {
   const FlossyScreenView({Key? key}) : super(key: key);
@@ -89,7 +90,7 @@ class FlossyScreenView extends StatelessWidget {
   }
 }
 
-class SameSizedBox extends StatelessWidget {
+class SameSizedBox extends ViewModelWidget<FlossyScreenViewModel> {
   const SameSizedBox({Key? key, required this.label, required this.icon}) : super(key: key);
 
   final String label;
@@ -98,27 +99,34 @@ class SameSizedBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Flexible(
-      child: Container(
-        height: 130.0,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey, width: 1.0),
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(30.0),
-          child: Column(
-            children: [
-              Icon(icon),
-              SizedBox(height: 10.0),
-              SizedBox(
-                width: 100.0,
-                child: Text(
-                  textAlign: TextAlign.center,
-                  label,
-                  style: TextStyle(fontWeight: FontWeight.bold),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(12.0),
+        onTap: () {
+          print("test");
+
+        },
+        child: Container(
+          height: 130.0,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey, width: 1.0),
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(30.0),
+            child: Column(
+              children: [
+                Icon(icon),
+                SizedBox(height: 10.0),
+                SizedBox(
+                  width: 100.0,
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    label,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
