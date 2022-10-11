9a5f268a5aede3e04f733d9ae5931d50cd6fd802
Danielle Kom <97759611+daniellekom@users.noreply.github.com>
2022-10-11 16:20:53 -0400

    more details view continued

--- lib/more_details/more_details_view.dart
+++ lib/more_details/more_details_view.dart
@@ -7,100 +7,98 @@ class MoreDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          LinearProgressIndicator(
-            color: Colors.orange,
-            value: 0.5,
-            backgroundColor: Colors.orange.withOpacity(0.3),
-            minHeight: 9.0,
-          ),
-          const Padding(
-            padding: EdgeInsets.all(25.0),
-            child: Text("Flossy", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Padding(
-            padding: EdgeInsets.all(25.0),
-            child: Text(
-              "Just a few more details...",
-              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-            ),
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                "Full Name",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+              LinearProgressIndicator(
+                color: Colors.orange,
+                value: 0.5,
+                backgroundColor: Colors.orange.withOpacity(0.3),
+                minHeight: 9.0,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Text("Flossy", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
               ),
               const SizedBox(
-                height: 10.0,
+                height: 30,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Text(
+                  "Just a few more details...",
+                  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
               ),
               Column(
                 children: [
-                  const TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      isDense: true,
-                    ),
+                  const Text(
+                    "Full Name",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
                   ),
                   const SizedBox(
-                    height: 30.0,
+                    height: 10.0,
+                  ),
+                  Column(
+                    children: [
+                      const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          isDense: true,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const Text(
+                    "Email",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
                   ),
                   TextFormField(
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                     ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              const Text(
-                "Email",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(
-                height: 100,
-              ),
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Padding(
-                          padding: EdgeInsets.all(20.0),
-                          child: Text("Show me dentists"),
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  ColoredBox(
+                    color: Colors.orange,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Show me dentists",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
                         ),
-                      ],
+                      ),
                     ),
                   ),
                 ],
               ),
             ],
           ),
-        ],
+        ),
       ),
-    )));
+    );
   }
 }
