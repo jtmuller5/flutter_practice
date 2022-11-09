cb1865f21220cef0baf90acd00f0728de25be7ad
Danielle Kom <97759611+daniellekom@users.noreply.github.com>
2022-11-09 09:24:37 -0500

    onboarding continued

--- lib/on_boarding_screen/widgets/on_boarding_confirm_button.dart
+++ lib/on_boarding_screen/widgets/on_boarding_confirm_button.dart
@@ -11,30 +11,35 @@ class OnboardingConfirmButton extends ViewModelWidget<OnboardingViewModel> {
   @override
   Widget build(BuildContext context, OnboardingViewModel viewModel) {
     return Center(
-
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              // style: BorderRadius(BorderRadius.circular(8),),
-              onPressed: () {
-                print("First name: ${viewModel.firstName}");
-                print(viewModel.lastName);
-                print(viewModel.email);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ToPage()),
-                );
-              },
-              child:const Text(
-                "See your Dentist Matches",
-                style: TextStyle(
-                  fontSize: 20,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            // style: BorderRadius(BorderRadius.circular(8),),
+            onPressed: () {
+              print("First name: ${viewModel.firstName}");
+              print(viewModel.lastName);
+              print(viewModel.email);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ToPage()),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
                 ),
-              )
-            )
-          ],
-        ),
-      );
+                backgroundColor: Colors.deepOrangeAccent),
+            child: const Text(
+              "See your Dentist Matches",
+              style: TextStyle(
+                fontSize: 20,fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
